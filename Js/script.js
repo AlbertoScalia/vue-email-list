f53661280d3dd154c38db5a36d1993fd9a87703c
AlbertoScalia <albscalia@gmail.com>
2023-05-17 15:43:42 +0200

    Feat (JS) : Add Ajax

--- Js/script.js
+++ Js/script.js
@@ -4,6 +4,24 @@ createApp({
     data() {
         return {
             mailAPI: 'https://flynn.boolean.careers/exercises/api/random/mail',
+            mailAddress: null,
+            mailList: []
         }
+    },
+    methods: {
+
+        getMailList() {
+            for (let i = 0; i < 10; i++) {
+                axios
+                    .get(this.mailAPI)
+                    .then(response => {
+                        this.mailAddress = response.data
+                        this.mailList.push(this.mailAddress)
+                    })
+            }
+        }
+    },
+    mounted() {
+        this.getMailList()
     }
 }).mount('#app')
